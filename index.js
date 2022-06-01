148353176b6ae0efd94894794c6837d04d7a9958
zameariislam <zameariislam@gmail.com>
2022-06-01 16:51:57 +0200

    accept and data storing

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
 let form = document.getElementById('form')
 let textInput = document.getElementById('textInput')
+let msg = document.getElementById('msg')
+let dateInput = document.getElementById('dateInput')
+let textArea = document.getElementById('textArea')
 
 form.addEventListener('submit', function (e) {
     e.preventDefault()
+    formValidation()
 
 
 })
+// form validation 
+
 let formValidation = () => {
     if (textInput.value === '') {
-        console.log('failure')
+        msg.innerText = 'Task can not be blank'
     }
     else {
-        console.log('success')
+
+        acceptData()
+
+        msg.innerText = ''
+
     }
+}
+let data = {}
+//  accecp and store data 
+let acceptData = () => {
+    data.taskName = textInput.value
+    data.description = textArea.value
+    data.date = dateInput.value
+    console.log(data)
+
+
+
 }
 
