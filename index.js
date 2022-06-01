204cc454e3eda74417fd79ce3d3a3962f9e035b7
zameariislam <zameariislam@gmail.com>
2022-06-01 17:29:24 +0200

    Reset data fields

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ let textInput = document.getElementById('textInput')
 let msg = document.getElementById('msg')
 let dateInput = document.getElementById('dateInput')
 let textArea = document.getElementById('textArea')
+let tasks = document.getElementById('tasks')
 
 form.addEventListener('submit', function (e) {
     e.preventDefault()
@@ -30,9 +31,27 @@ let acceptData = () => {
     data.taskName = textInput.value
     data.description = textArea.value
     data.date = dateInput.value
+    createTask(data);
+
+}
+const createTask = (data) => {
     console.log(data)
+    tasks.innerHTML +=
+        ` <div>
+    <span class="fw-bold"> ${data.taskName}</span>
+    <span class="small text-secondary">${data.date}</span>
+    <p>${data.description}</p>
+    <span class="options">
+        <i class="fa-solid fa-pen-to-square"></i>
+        <i class="fa-solid fa-trash-can"></i>
+    </span>
 
+</div>`
 
+    //   Reset  Data fields 
 
+    textInput.value = '';
+    dateInput.value = ''
+    textArea.value = ''
 }
 
