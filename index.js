570eb4e2ded064ca67176b8a4ef8c182c7f49b88
zameariislam <zameariislam@gmail.com>
2022-06-02 06:03:04 +0200

    edit added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ let msg = document.getElementById('msg')
 let dateInput = document.getElementById('dateInput')
 let textArea = document.getElementById('textArea')
 let tasks = document.getElementById('tasks')
+let add = document.getElementById('add')
 
 form.addEventListener('submit', function (e) {
     e.preventDefault()
@@ -16,10 +17,17 @@ form.addEventListener('submit', function (e) {
 let formValidation = () => {
     if (textInput.value === '') {
         msg.innerText = 'Task can not be blank'
+
     }
     else {
 
         acceptData()
+        add.setAttribute("data-bs-dismiss", "modal");
+        add.click();
+
+        (() => {
+            add.setAttribute("data-bs-dismiss", "");
+        })();
 
         msg.innerText = ''
 
@@ -28,6 +36,7 @@ let formValidation = () => {
 let data = {}
 //  accecp and store data 
 let acceptData = () => {
+
     data.taskName = textInput.value
     data.description = textArea.value
     data.date = dateInput.value
@@ -35,15 +44,16 @@ let acceptData = () => {
 
 }
 const createTask = (data) => {
-    console.log(data)
+
     tasks.innerHTML +=
         ` <div>
     <span class="fw-bold"> ${data.taskName}</span>
     <span class="small text-secondary">${data.date}</span>
     <p>${data.description}</p>
     <span class="options">
-        <i class="fa-solid fa-pen-to-square"></i>
-        <i class="fa-solid fa-trash-can"></i>
+        <i  data-bs-toggle="modal"
+        data-bs-target="#form" onClick='editTask(this)' class="fa-solid fa-pen-to-square"></i>
+        <i onClick='deleteTask(this)' class="fa-solid fa-trash-can"></i>
     </span>
 
 </div>`
@@ -53,5 +63,25 @@ const createTask = (data) => {
     textInput.value = '';
     dateInput.value = ''
     textArea.value = ''
+
 }
+let deleteTask = (el) => {
+    let element = (el.parentElement.parentElement)
+    element.remove()
+
+}
+let editTask = (el) => {
+    let element = el.parentElement.parentElement
+    textInput.value = element.children[0].innerText
+    dateInput.value = element.children[1].innerText
+    textArea.value = element.children[2].innerText
+    element.remove()
+
+}
+
+
+
+
+
+
 
